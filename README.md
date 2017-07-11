6114061d5672cf03af334aaa4a6df5ad8079bc6b
asclepiusgal <jeanna.e.clark@gmail.com>
2017-07-11 09:19:27 -0700

    code review

--- README.md
+++ README.md
@@ -1,8 +1,53 @@
-Launchcode homework building a blog style website using flask, SQLAlchemy, Jinja2, pymysql
+# Blog App
+## A simple Flask project for LC-101 students
 
-#sources
-Launchcode Database configuration part 2
-https://www.youtube.com/watch?v=-bvlj_3Im6s
+# _Implements_:
 
-Build Python Web App using Python and Flask
-https://www.youtube.com/watch?v=meu0ITheGBY&t=19506s
+- [Flask](http://flask.pocoo.org/) template rendering
+- manual form validation
+- HTTP routing & endpoints
+- HTML
+- CSS
+- request
+- redirect
+- render_template (in place of jinja2)
+- mySQL database
+- SQLAlchemy 
+- cookie sessions
+
+***
+
+# SQLAlchemy setup
+
+open python3 in terminal
+>>> from main import db,Task 
+>>> db
+>>> db.create_all()  ### creates the DB using the specified columns
+>>> new_task = Task('finish Lesson 2')
+>>> db.session.add(new_task) 
+>>> new_task_2 = Task('finish Lesson 3')
+>>> db.session.add(new_task_2) 
+>>> db.session.commit() ### adds the new_task variable to database
+>>> tasks = task.query.all() ### selects everything from the database
+>>> tasks[0].name = 'finish Lesson 2'
+>>> db.session.delete(new_task_2)
+>>> db.drop_all() ### drops all the tables
+>>> db.create_all() ### creates the database with the new / added columns
+
+open mysql in another terminal window 
+> use blog; ## uses the blog database
+> show tables; ## shows the tables
+> describe task; ## shows the table columns
+> select * from task; ## shows the new data inserted into the table columns
+
+***
+
+## _Sources_
+
+- _Project rubric_: [LC-101 Blog App](http://education.launchcode.org/web-fundamentals/assignments/build-a-blog/)
+- _Video supplement on Flask_: [Build Python Web App using Python and Flask](https://www.youtube.com/watch?v=meu0ITheGBY&t=19506s)
+- _Video supplement on databases_: [Launchcode Database configuration part 2](https://www.youtube.com/watch?v=-bvlj_3Im6s)
+
+***
+
+(c) June 2017
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python
+
+__author__ = "student"
+__version__ = "1.0"
+# June 26, 2017
+# Flask Blog App re: LaunchCode lc101
+# Rubric: http://education.launchcode.org/web-fundamentals/assignments/build-a-blog/
+
+
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-# import os deplaying with heroku
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:enterbab@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -15,26 +22,31 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(1500))
+    date = db.Column(db.DateTime)
 
-    def __init__(self, title, content):
+    def __init__(self, title, content, date=None):
         self.title = title
         self.content = content
+        if date is None:
+            date = datetime.utcnow()
+        self.date = date
 
 
-# TODO: #landing page redirected to blog
+# landing page redirected to blog
 @app.route('/')
 def index():
     return redirect('/blog')
 
 
-# TODO: redirected from / showing all blogs
+# redirected from / showing all blogs
 @app.route('/blog', methods=['GET'])
 def blog():
+    # TODO: with the date timestamp added to blog entries, you can add a query parameter to sort by date descending
     blogs = Blog.query.all()
     return render_template('blog.html', title='Your Blog', blogs=blogs)
 
 
-# TODO: query all blogs and return/gets the selected blog
+# query all blogs and return/gets the selected blog
 @app.route('/selected_blog', methods=['GET'])
 def selected_blog():
     blog_id = request.args.get('id')
@@ -42,7 +54,7 @@ def selected_blog():
     return render_template('selected_blog.html', selected_blog=blog_post)
 
 
-# TODO: new post and check for errors
+# new post and check for errors
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     # currently no errors
